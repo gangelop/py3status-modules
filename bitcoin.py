291af9697dc6ff7439a41e4e11780cbde107c3ce
George Angelopoulos <l4than.d3vers@gmail.com>
2013-05-29 20:06:14 +0300

    added bitcoin.py
    Displays the latest price from bitstamp.net

---
+++ bitcoin.py
@@ -0,0 +1,23 @@
+class Py3status:
+
+
+    def reddit(self, json, i3status_config):
+        """
+        Get and display link and comment karma from reddit.
+        """
+        response = {'full_text':'BTC: ', 'name':'bitcoin', 'color':'#ffffff' }
+
+        try:
+            import json
+            import requests
+
+            r = requests.get('https://www.bitstamp.net/api/ticker/')
+
+            ticker = json.loads(r.text)
+
+            response['full_text'] += ticker['last'] + '$'
+
+        except Exception as e:
+            response['color'] = '#ff0000'
+        finally:
+            return (0, response)
